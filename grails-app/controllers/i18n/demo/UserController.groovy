2dc5f5ebdcbecc1b071ca971c3a30666b5dfe29b
Gaspar Medina <gaspar.medina@peya.local>
2017-02-25 17:14:51 -0300

    Initial Commit

---
+++ grails-app/controllers/i18n/demo/UserController.groovy
@@ -0,0 +1,19 @@
+package i18n.demo
+
+import grails.converters.JSON
+import org.springframework.web.servlet.support.RequestContextUtils
+
+class UserController {
+
+    def messageSource
+    def index(){
+		Locale locale=RequestContextUtils.getLocale(request)
+		String msg=messageSource.getMessage("user.login.success.message", null, locale)
+    }
+
+    def login() {
+        Locale locale=RequestContextUtils.getLocale(request)
+        String msg=messageSource.getMessage("user.login.success.message", null, locale)
+        render msg
+    }
+}
